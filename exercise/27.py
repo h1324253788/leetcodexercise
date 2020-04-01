5045d54ca2b1bead91b9a54084a3dc1e472d0d5d
“h1324253788” <1324253788>
2020-04-01 11:52:12 +0800

    27题

---
+++ exercise/27.py
@@ -0,0 +1,38 @@
+"""
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+给定数组 nums = [1,1,2],
+
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+
+"""
+
+
+# 删除法
+def removeDuplicates(self, nums: List[int]) -> int:
+    i = 1
+    while i <= len(nums) - 1:
+        if nums[i] == nums[i - 1]:
+            del nums[i]
+        else:
+            i += 1
+
+    return len(nums)
+
+
+# 双指针法
+def removeDuplicates(self, nums: List[int]) -> int:
+    j = 0
+    if len(nums) == 0:
+        return 0
+    for i in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            j + 1
+            nums[j] = nums[i]
+    return j + 1
